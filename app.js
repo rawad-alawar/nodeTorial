582f4204a570c073f6518695dfc94087b60bf40a
Rawad Alawar <email": null>
2016-04-16 00:24:48 +1200

    connectes db and gets page looking good

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongo = require('mongodb')
+var db = require('monk')('localhost/nodeTorial')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +23,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//very important to make db accessible to rest of files
+app.use(function(req, res, next) {
+  req.db =  db;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb')
+var db = require('monk')('localhost/nodeTorial')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  var db = req.db   //db equals the database, req.db comes from app.js
+  var entries = db.get('entries')
+  entries.find({},{},function(err, entries){
+    if (err) throw error
+    res.render('index', {'entries': entries})
+  })
 });
 
 module.exports = router;
