cff5029827f5fce87816cc6f4e06ae81a80bc558
Rawad Alawar <email": null>
2016-04-20 16:31:41 +1200

    test monk

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongo = require('mongodb')
-var db = require('monk')(process.env.MY_MONGO_URI)
+var db = require('monk')(process.env.MY_MONGO_URI) //('localhost/nodeTorial')
 
 var routes = require('./routes/index');
 var blogpage = require('./routes/blogpage');
